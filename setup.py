645e31e2a2a8efd71bccd8f7c92f9e784414e25e
Konstantin Schubert <mail@konstantinschubert.com>
2018-06-02 21:49:18 +0200

    Add missing dependency

--- setup.py
+++ setup.py
@@ -2,22 +2,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='zero',
-    version='0.0.1',
+    name="zero",
+    version="0.0.1",
     description="Unlimited local storage",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-    keywords='',
-    author='Konstantin Schubert',
+    keywords="",
+    author="Konstantin Schubert",
     packages=find_packages(),
-    install_requires=[
-        'fusepy',
-    ],
+    install_requires=["fusepy", "b2"],
     include_package_data=True,
     zip_safe=True,
-    entry_points={
-        'console_scripts': ['zero = zero.main:main',],
-    }
+    entry_points={"console_scripts": ["zero = zero.main:main"]},
 )
