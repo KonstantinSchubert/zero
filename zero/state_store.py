8d53dfa71dcd75d7b56f46bff01ed089eef16eaa
Konstantin Schubert <mail@konstantinschubert.com>
2018-07-02 13:18:42 +0200

    Remove stale duplicate locking code

--- zero/state_store.py
+++ zero/state_store.py
@@ -1,12 +1,7 @@
 import sqlite3
-import time
 
 
 class IllegalTransitionException(Exception):
-    pass
-
-
-class InodeLockedException(Exception):
     pass
 
 
@@ -26,66 +21,6 @@ class StateStore:
             self.connection.execute(
                 """CREATE TABLE IF NOT EXISTS states (inode integer primary key, state text)"""
             )
-        with self.connection:
-            self.connection.execute(
-                """CREATE TABLE IF NOT EXISTS locks (inode integer primary key)"""
-            )
-
-    class Lock:
-
-        def __init__(self, state_store, inode, acquisition_max_retries=0):
-            self.acquisition_max_retries = acquisition_max_retries
-            self.inode = inode
-            self.state_store = state_store
-
-        def __enter__(self):
-            # Lock database while setting lock
-            if self._try_locking():
-                return
-            for counter in range(self.acquisition_max_retries):
-                time.sleep(1.)
-                # 1000 ms - We wait this long because a big upload might be locking
-                # TODO: Reduce likelihood of huge uploads locking.
-                # For example, when big files are written, the worker should avoid uploading
-                # while the files is still being written. This is not a stric rule, more of a performence consideration
-                if self._try_locking():
-                    return
-            raise InodeLockedException
-
-        def __exit__(self, *args):
-            with self.state_store.connection:
-                self.state_store.connection.execute("""BEGIN IMMEDIATE""")
-                assert self.state_store._is_locked(self.inode)
-                self.state_store._unlock(self.inode)
-                print(f"unlocked {self.inode}")
-
-        def _try_locking(self):
-            print("TRY LOCKING")
-            with self.state_store.connection:
-                # We must disallow any other process from even reading the
-                # lock situation before we read and then change the lock situation
-                self.state_store.connection.execute("""BEGIN IMMEDIATE""")
-                if not self.state_store._is_locked(self.inode):
-                    self.state_store._lock(self.inode)
-                    print(f"locked {self.inode}")
-                    return True
-                return False
-
-    def _is_locked(self, inode):
-        cursor = self.connection.execute(
-            """SELECT * FROM locks WHERE inode = ? """, (inode,)
-        )
-        return cursor.fetchone() is not None
-
-    def _lock(self, inode):
-        self.connection.execute(
-            """INSERT INTO locks (inode) VALUES (?)""", (inode,)
-        )
-
-    def _unlock(self, inode):
-        self.connection.execute(
-            """DELETE FROM locks WHERE inode = ?""", (inode,)
-        )
 
     def set_remote(self, inode):
         with self.connection:
