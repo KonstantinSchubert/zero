cd923d524cf182dabf1b9a6d76831919429be98a
Konstantin Schubert <mail@konstantinschubert.com>
2018-08-23 01:10:22 +0200

    Drafting more tests

--- zero/tests/integration_tests/test_operations.py
+++ zero/tests/integration_tests/test_operations.py
@@ -1,12 +1,10 @@
 from unittest import TestCase
 from fuse import FuseOSError
+from unittest.mock import MagicMock
 
-from zero.b2_api import FileAPI
-from zero.b2_file_info_store import FileInfoStore
-from zero.main import get_config
 from zero.operations import Filesystem
 
-from .configure import TestingContext, DB_PATH
+from .configure import TestingContext
 
 PATH = "yo"
 FILE_CONTENT = b"Some file content"
@@ -14,23 +12,16 @@ FILE_CONTENT = b"Some file content"
 
 class OperationTest(TestCase):
 
-    def setUp(self):
-        file_info_store = FileInfoStore(DB_PATH)
-        config = get_config()
-        self.api = FileAPI(
-            file_info_store=file_info_store,
-            account_id=config["accountId"],
-            application_key=config["applicationKey"],
-            bucket_id=config["bucketId"],
-        )
-        self.context = TestingContext(api=self.api)
-        self.filesystem = Filesystem(self.context.cache)
-
     def create_local_file(self):
         return self.context.create_file(PATH, FILE_CONTENT)
 
 
 class AccessTest(OperationTest):
+
+    def setUp(self):
+        self.api = MagicMock()
+        self.context = TestingContext(api=self.api)
+        self.filesystem = Filesystem(self.context.cache)
 
     def test_access_local_file(self):
 
@@ -42,23 +33,13 @@ class AccessTest(OperationTest):
         with self.assertRaises(FuseOSError):
             self.filesystem.access(path, 7)
 
-    def test_access_remote_file(self):
-        # create a local file with default permissions
-        path = self.create_local_file()
-        # make file remote
-        todo
-        # assert that a specific access call succeeds
-        todo
-        # assert sure another access call fails
-        with assertRaises(FuseOSError):
-            self.filesystem.access(path, ??)
-
-    def test_access_permissions_persistet(self):
-        todo
-        pass
-        # create a local file
-        # set weird ownership and permissions
-        # make file remote
-        # assert that ownership and permissions are the same still
-        # make file local
-        # assert that ownership and permissions are the same still
+    # def test_access_remote_file(self):
+    #     # create a local file with default permissions
+    #     path = self.create_local_file()
+    #     # make file remote
+    #     todo
+    #     # assert that a specific access call succeeds
+    #     todo
+    #     # assert sure another access call fails
+    #     with assertRaises(FuseOSError):
+    #         self.filesystem.access(path, ??)
